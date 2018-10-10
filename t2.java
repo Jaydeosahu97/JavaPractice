92e57b260219d6e91b70e6da3364803f3b0d8a7e
White Mars <aakash.saini3598@gmail.com>
2018-10-10 22:48:55 +0530

    Update t2.java
    Created the instance of each class

--- t2.java
+++ t2.java
@@ -1,15 +1,22 @@
-class t2{
-	class t4{
-		static class t3{
-			public static void doSome(){
+class t2
+{
+	class t4
+	{
+		class t3
+		{
+			public void doSome()
+			{
 				System.out.println("help");
 			} 
 		}
-		static t3 obj = new t3();
+		 
 	}
-	static t4 done = new t4();
-	public static void main(String[] args) {
-			//here i'm trying to achieve the access of method doSome which is within the class t3 nested in class t4
-			done.obj.doSome();
+	
+	public static void main(String[] args) 
+		{
+			t2 y = new t2();
+			t2.t4 x = y.new t4();
+			t2.t4.t3 obj = x.new t3();
+			obj.doSome();
 		}
 }	
